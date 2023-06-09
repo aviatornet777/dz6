d080cada079dbcb8cb4209a1f1b489a59bf48a8d
Serg <Serg.srav@mail.ru>
2023-06-09 23:51:31 +0500

    домашнее задание 6

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//         Задача 43: Напишите программу, которая найдёт точку 
+//         пересечения двух прямых, заданных уравнениями
+//          y = k1 * x + b1, y = k2 * x + b2; 
+//          значения b1, k1, b2 и k2 задаются пользователем.
+
+// Console.WriteLine("Введите b1");
+// int b1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите k1");
+// int k1 = Convert.ToInt32(Console.ReadLine());
+
+
+// Console.WriteLine("Введите b2");
+// int b2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите k2");
+// int k2 = Convert.ToInt32(Console.ReadLine());
+
+//  double a=b2-b1;
+//  double c=k1-k2;
+//  double x=a/c;
+//  double y=k1*x+b1;
+//  Console.Write(x);
+//  Console.Write(y);
+
+
+
+
